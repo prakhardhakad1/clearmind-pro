4fc8e325e1dfb4920651f4263b9c985ae8f6d2c9
Prakhar <prakhar@example.com>
2026-09-12 18:24:32 +0530

    Test isolated serverless endpoint

--- api/index.py
+++ api/index.py
@@ -1,19 +1,8 @@
-import sys
+﻿import sys
 import os
 
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
 
-try:
-    from main import app
-except BaseException as e:
-    import traceback
-    err_trace = traceback.format_exc()
-    from fastapi import FastAPI
-    from fastapi.responses import PlainTextResponse
-    app = FastAPI()
-    @app.get("/")
-    @app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
-    async def catch_all(path: str = ""):
-        return PlainTextResponse(f"Startup Crash Traceback:\n{err_trace}", status_code=500)
+from main import app
